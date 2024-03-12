d0a540c48e17906fcbf23808121864ef02dc9c77
InnovationPARK <innovation0220@gmail.com>
2024-03-12 17:50:34 +0900

    0312 last

--- openai_tutorial.py
+++ openai_tutorial.py
@@ -21,8 +21,8 @@ response = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
     {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": "Who won the world series in 2020?"},
-    {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+    {"role": "user", "content": "How did Korea do in the 2002 World Cup?"},
+    {"role": "assistant", "content": "Korea achieved its highest ever performance in the 2002 World Cup."},
     {"role": "user", "content": "Where was it played?"}
   ]
 )
--- openai_tutorial_json.py
+++ openai_tutorial_json.py
@@ -6,7 +6,9 @@ response = client.chat.completions.create(
   response_format={ "type": "json_object" },            #응답할때 json
   messages=[
     {"role": "system", "content": "You are a helpful assistant designed to output JSON. 너는 고객의 후기를 바탕으로 그들의 만족도를 조사하는 최고의 조사원이야."},
-    {"role": "user", "content": "오늘 구매한 컴퓨터가 정말 맘에 안들어요. 소음이 심해요"}
+    {"role": "user", "content": "오늘 구매한 컴퓨터가 정말 맘에 안들어요. 소음이 심해요."},
+    {"role": "user", "content": "오늘 구매한 컴퓨터가 너무 좋아요 속도가 빨라요. 행복해요."},
+    {"role": "user", "content": "오늘 구매한 컴퓨터가 정말 좋아요. 가성비가 좋아서 아들이 좋아해요."}
   ]
 )
 print(response.choices[0].message.content)
